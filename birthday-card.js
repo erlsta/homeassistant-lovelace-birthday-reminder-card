0d89146060365d42ce1f9e0ac605d0088e4669af
Serios <seriosbg@gmail.com>
2022-01-19 23:32:48 +0200

    Update birthday-card.js
    Code refactoring and adding functionalities.

--- birthday-card.js
+++ birthday-card.js
@@ -1,59 +1,63 @@
+console.info(`%c Birthday card \n%c Version: ${'1.3.0'} `, 'color: orange; font-weight: bold; background: black', 'color: white; font-weight: bold; background: dimgray');
+
 class BirthdayCard extends HTMLElement {
+
+	setConfig(config) {
+		if (!config) {
+			throw new Error('Card config incorrect');
+		}
+		this._config = config;
+		this._birthdayList = [];
+		this._bdIcons = {'b':'mdi:cake', 'm':'mdi:cross', 'e':'mdi:ring', 'w':'mdi:human-male-female', 'misc':'mdi:calendar-blank-outline' } //set icons for event type: b - birthday, m - memorial, e - engadgment, w - wedding, misc - when type is missing or not in the array  
+		this._datelocale = config.date_locale ? config.date_locale : 'default'; // years
+		this._bdTextToday = config.text_today ? config.text_today : 'Today'; // Today
+		this._bdTextTomorrow = config.text_tomorrow ? config.text_tomorrow : 'Tomorrow'; // Tomorrow
+		this._bdTextNone = config.text_none ? config.text_none : 'No birthdays in the next'; // No birthdays during next
+		this._bdTextDays = config.text_days ? config.text_days : 'days'; // days
+		this._bdTextYears = config.text_years ? config.text_years : 'years'; // years
+		
+		this.renderCard();
+	}
+
 	set hass(hass) {
-		
-// Birthday-calendar v1.2 (10.03.2019)		
+		this._hass = hass;
+	}
 
-
-///// SETTINGS /////////////////////////////////////////////////////////////
+	async renderCard() {
+		const config = this._config;
 		
+		if (window.loadCardHelpers) {
+		  this.helpers = await window.loadCardHelpers();
+		}
 		
-		// Settings //
-		
-		var bdDeadSymbol = "&#8224;"; // (Symbol for people who have passed on - set «, d:1» in birthday list)
-		var bdMarriedSymbol = "&#9829;";
-		
-		
-		// String translations (translate these to your own language) //
-		
-		var bdTextToday = "Today"; // Today
-		var bdTextTomorrow = "Tomorrow"; // Tomorrow
-		var bdTextNone = "No birthdays in the next"; // No birthdays during next
-		var bdTextDays = "days"; // days
-		var bdTextYears = "years"; // years
-		var bdTextIn = ""; // in
-		
-		
-///// BIRTHDAY REGISTRY ////////////////////////////////////////////////////
-	
-		
-		var birthdayList=[
-			{name:"Adam", day:17, month:2, year:1990},
-			{name:"Amanda", day:2, month:3, year:1967},
-			{name:"Grandfather", day:16, month:2, year:1927, s:1},
-			{name:"Gladys", day:10, month:2, year:1967},
-			{name:"Peter", day:7, month:3, year:1967},
-			{name:"Wedding aniversary", day:5, month:3, year:2003, s:2},
-		];
-		
-		
-///// DO NOT EDIT BELOW THIS LINE //////////////////////////////////////////
-		
-		
+		// Create the card
 		if (!this.content) {
 			const card = document.createElement('ha-card');
-			var tittel = this.config.title;
-			card.header = tittel ? tittel : "Birthdays"; // Card title from ui-lovelace.yaml - Defaults to Birthdays
+			card.header = config.title ? config.title : "Birthdays"; // Card title from ui-lovelace.yaml - Defaults to Birthdays
 			this.content = document.createElement('div');
 			this.content.style.padding = '0 16px 16px';
 			card.appendChild(this.content);
 			this.appendChild(card);
 		}
 		
-		const entityId = this.config.entity;
-		const state = hass.states[entityId];
+		const entityId = config.entity;
+		const state = this._hass.states[entityId];
 		const stateStr = state ? state.state : 'unavailable';
-		const numberOfDays = this.config.numberofdays ? this.config.numberofdays : 14; //Number of days from today upcomming birthdays will be displayed - default 14
+		const numberOfDays = config.numberofdays ? config.numberofdays : 14; //Number of days from today upcomming birthdays will be displayed - default 14
+		var events_list = config.events_list;
 		
+		if (config && Array.isArray(events_list) ) {
+			events_list.forEach((event) => {
+				this._birthdayList.push({
+					'name': event.name,
+					'day': event.day,
+					'month': event.month,
+					'year': event.year ? event.year : 0,
+					'icon': event.type ? this._bdIcons[event.type] : this._bdIcons.misc,
+					'type': event.type
+				});
+			});
+		}
 		
 		var current = new Date();
 		var currentMonth = current.getMonth();
@@ -63,8 +67,8 @@ class BirthdayCard extends HTMLElement {
 		var oneDay = 24*60*60*1000;
 		
 		
-		for(var i = 0; i < birthdayList.length; i++) {
-			var obj = birthdayList[i];
+		for(var i = 0; i < this._birthdayList.length; i++) {
+			var obj = this._birthdayList[i];
 			
 			if ( ((obj.month-1) < currentMonth) || ( ((obj.month-1) == currentMonth) && (obj.day < currentDay) ) ) {
 				// Birthday passed in current year - add one year to throw date to next birthday
@@ -81,7 +85,7 @@ class BirthdayCard extends HTMLElement {
 			if ( obj.diff > numberOfDays) { obj.ts = 0; }
 		}
 		
-		var sortertListe = birthdayList.sort((a, b) => (a.ts > b.ts) ? 1 : ((b.ts > a.ts) ? -1 : 0)); 
+		var sortertListe = this._birthdayList.sort((a, b) => (a.ts > b.ts) ? 1 : ((b.ts > a.ts) ? -1 : 0)); 
 		var birthdayToday = "";
 		var birthdayNext = "";
 		
@@ -90,29 +94,44 @@ class BirthdayCard extends HTMLElement {
 			var obj = sortertListe[i];
 			
 			if (obj.year > 0) {
-				var age = "(" + (currentYear - obj.year + obj.aPlus) + " " + bdTextYears + ")";
+				var age = "(" + (currentYear - obj.year + obj.aPlus) + " " + this._bdTextYears + ")";
 			} else {
 				var age = "";
 			}
 			
-			var bdSymbol = "";
-			if (obj.s == 1) { bdSymbol = " " + bdDeadSymbol; }
-			if (obj.s == 2) { bdSymbol = " " + bdMarriedSymbol; }
-			
+			var bdTodaySymbol = (obj.type == 'm') ? 'mdi:candle' : 'mdi:party-popper';
+			var bdDate = Intl.DateTimeFormat(this._datelocale, { month: 'short', day: 'numeric' }).format(new Date(currentYear, obj.month-1, obj.day));
+				
 			if (((obj.month-1) == currentMonth) && (obj.day == currentDay)) {
 				
-				birthdayToday = birthdayToday + "<div class='bd-wrapper bd-today'><ha-icon class='ha-icon entity on' icon='mdi:crown'></ha-icon><div class='bd-name'>" + obj.name + " " + age + bdSymbol + "</div><div class='bd-when'>" + bdTextToday + "</div></div>";
+				birthdayToday = birthdayToday + "<div class='bd-wrapper bd-today'><ha-icon class='ha-icon entity on' icon='" + bdTodaySymbol + "'></ha-icon><div class='bd-name'>" + obj.name + " " + age + "</div><div class='bd-when'>" + this._bdTextToday + "</div></div>";
 				
 			} else if (obj.ts != 0) {
 				
-				var dbExpr = obj.diff == 1 ? bdTextTomorrow : bdTextIn + " " + obj.diff + " " + bdTextDays;
-				birthdayNext = birthdayNext + "<div class='bd-wrapper'><ha-icon class='ha-icon entity' icon='mdi:calendar-clock'></ha-icon><div class='bd-name'>" + obj.name + " " + age + bdSymbol + "</div><div class='bd-when'>" + dbExpr + " (" + obj.day + "." + obj.month + ")</div></div>";
+				var dbExpr = obj.diff == 1 ? this._bdTextTomorrow  : obj.diff + " " + this._bdTextDays;
+				birthdayNext = birthdayNext + "<div class='bd-wrapper'><ha-icon class='ha-icon entity' icon='" + obj.icon + "'></ha-icon><div class='bd-name'>" + obj.name + " " + age + "</div><div class='bd-when'>" + dbExpr + " ( " + bdDate + " )</div></div>";
 				
 			}
 		}
 		
+		if (!birthdayToday && !birthdayNext) {
+			var cardHtmlContent = "<div class='bd-none'>" + this._bdTextNone + " " + numberOfDays + " " + this._bdTextDays + "</div>";
+		} else if (!birthdayToday) {
+			var cardHtmlContent = birthdayNext;
+		} else if (!birthdayNext) {
+			var cardHtmlContent = birthdayToday;
+		} else {
+			var cardHtmlContent = birthdayToday + "<div class='bd-divider'></div>" + birthdayNext;
+		}
 		
-		var cardHtmlStyle = `
+		this.content.innerHTML = this.style() + cardHtmlContent;		
+		
+	}
+
+	
+	//set card style
+	style() {	
+		return `
 		<style>
 			.bd-wrapper {
 				padding: 5px;
@@ -135,12 +154,12 @@ class BirthdayCard extends HTMLElement {
 				height: 20px;
 				width: 20px;
 				margin-left: 5px;
-				margin-right: 17px;
+				margin-right: 5px;
 				color: var(--paper-item-icon-color);
 			}
 			.bd-wrapper .ha-icon.on {
 				margin-left: 5px;
-				margin-right: 17px;
+				margin-right: 5px;
 				color: var(--paper-item-icon-active-color);
 			}
 			.bd-name {
@@ -159,26 +178,8 @@ class BirthdayCard extends HTMLElement {
 			}
 		</style>
 		`;
-		
-		if (!birthdayToday && !birthdayNext) {
-			var cardHtmlContent = "<div class='bd-none'>" + bdTextNone + " " + numberOfDays + " " + bdTextDays + "</div>";
-		} else if (!birthdayToday) {
-			var cardHtmlContent = birthdayNext;
-		} else if (!birthdayNext) {
-			var cardHtmlContent = birthdayToday;
-		} else {
-			var cardHtmlContent = birthdayToday + "<div class='bd-divider'></div>" + birthdayNext;
-		}
-		
-		this.content.innerHTML = cardHtmlStyle + cardHtmlContent;
-		
 	}
 	
-	
-	
-	setConfig(config) {
-		this.config = config;
-	}
 	
 // The height of your card. Home Assistant uses this to automatically distribute all cards over the available columns.
 	getCardSize() {
